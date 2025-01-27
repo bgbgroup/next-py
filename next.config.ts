b0f4f954af48e5caf6b8ca67791ff99f8cce9ca8
Jalal Fathi <jfathi.mail@gmail.com>
2025-01-27 11:39:34 +0000

    papi

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,17 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: '/api/py/:path*',
+        source: '/papi/:path*',
         destination:
           process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:5328/api/py/:path*'
-            : '/api/py/:path*',
+            : '/api/py/',
       },
     ]
   },
+  experimental: {
+		esmExternals: "loose",
+	},
 }
 
 export default nextConfig
