646332a81ccb2a78dd138ec0a389f55b9184381d
Jalal Fathi <jfathi.mail@gmail.com>
2025-01-30 14:03:03 +0000

    back to start

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,8 @@ const nextConfig = {
         source: '/papi/:path*',
         destination:
           process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5328/papi/:path*'
-            : '/api/',
+            ? 'http://127.0.0.1:5328/api/py/:path*'
+            : '/api/py/',
       },
     ]
   },
---
+++ src/api/ts/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server';
+
+export async function GET(req: Request) {
+  return NextResponse.json({
+    message: 'Hello World',
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
